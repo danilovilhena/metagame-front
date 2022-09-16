b1542f68971845298685fe9cb95b5aa99a05859f
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2022-09-15 22:15:42 -0300

    Add redux store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Home from './routes/Home';
+import store from './store';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home/>} />
+				</Routes>
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
