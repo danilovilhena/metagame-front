8b0442ddd13177cacaec4b61fc62fdeb525d72b3
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2022-10-20 23:35:39 -0300

    Update default theme

--- theme.js
+++ theme.js
@@ -1,12 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
-// 2. Add your color mode config
 const config = {
-	initialColorMode: 'light',
-	useSystemColorMode: false,
+	initialColorMode: 'system',
+	useSystemColorMode: true,
 };
 
-// 3. extend the theme
 const theme = extendTheme({ config });
 
 export default theme;
