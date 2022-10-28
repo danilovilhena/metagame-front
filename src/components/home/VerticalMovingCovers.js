47c31aa165501c0787879344bb6ea19df27d4cc1
Vicente Mattos <vicentemattos1@hotmail.com>
2022-10-28 19:01:33 -0300

    Infinite slider scroll

--- src/components/home/VerticalMovingCovers.js
+++ src/components/home/VerticalMovingCovers.js
@@ -1,6 +1,6 @@
 import {
 	Box,
-	Stack,
+	Flex,
 	Image,
 	useBreakpointValue,
 	keyframes,
@@ -17,41 +17,40 @@ export default function VerticalMovingCovers({ getCover }) {
 	from {opacity: 0}
 	to {opacity: 1}
   `;
-
 	return (
-		<Box
-			maxW={{ base: '100%', lg: '456px' }}
-			maxH="100%"
-			overflow="hidden"
-			my={{ base: '2rem', lg: 0 }}
-		>
-			<Stack direction={{ base: 'column', lg: 'row' }} spacing="1rem">
-				{[...Array(isWideVersion ? 4 : 3)].map((_, idx) => (
-					<Stack
-						className={
-							idx % 2 === 0 ? styles['row-animate'] : styles['row-animate-i']
-						}
-						direction={{ base: 'row', lg: 'column' }}
-						spacing="1rem"
-						key={`cover-col-${idx}`}
-					>
-						{[...Array(10)].map((_, innerIdx) => (
-							<Image
-								animation={`${easeIn} 6s`}
-								w="100px"
-								minH="150px"
-								objectFit="cover"
-								src={getCover(idx, innerIdx)}
-								alt="cover"
-								mb="0.5em"
-								borderRadius="0.5rem"
-								key={`cover-movie-${innerIdx}`}
-								fallbackSrc="movie-cover.svg"
-							/>
-						))}
-					</Stack>
-				))}
-			</Stack>
-		</Box>
+		<Flex>
+			{[...Array(isWideVersion ? 4 : 3)].map((_, idx) => (
+				<Box
+					maxH="100%"
+					overflow="hidden"
+					my={{ base: '2rem', lg: 0 }}
+					className={styles['slider-body']}
+					key={idx}
+				>
+					<div className={styles['slider']}>
+						<div
+							className={
+								idx % 2 == 0 ? styles['slide-track'] : styles['slide-track-b']
+							}
+						>
+							{[...Array(10)].map((_, innerIdx) => (
+								<Image
+									animation={`${easeIn} 6s`}
+									w="100px"
+									minH="150px"
+									objectFit="cover"
+									src={getCover(idx, innerIdx)}
+									alt="cover"
+									mb="0.5em"
+									borderRadius="0.5rem"
+									key={`cover-movie-${innerIdx}`}
+									fallbackSrc="movie-cover.svg"
+								/>
+							))}
+						</div>
+					</div>
+				</Box>
+			))}
+		</Flex>
 	);
 }
