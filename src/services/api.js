bdeb1fe3480c295710b71b5c6c8981e62044b069
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2023-05-25 13:19:22 -0300

    Push new api url

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import { getSession, signOut } from 'next-auth/react';
 const baseURL =
 	process.env.NODE_ENV === 'development'
 		? 'http://127.0.0.1:8000/api'
-		: 'http://18.229.107.10:8000/api';
+		: 'https://metagame-load-balancer-1799570626.sa-east-1.elb.amazonaws.com/api/';
 
 const apiClient = () => {
 	const api = axios.create({ baseURL: baseURL });
